b03ae869c4280b01979253c4f3bdaebbfa8e56df
Corey Butler <corey@coreybutler.com>
2016-01-22 16:41:39 -0600

    Updated npm package.

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-module.exports = function (callback) {
+module.exports = function (component, callback) {
   if (document) {
     var s = document.createElement('script')
     s.setAttribute('type', 'text/javascript')
-    s.setAttribute('src', require('path').join(__dirname, 'dist/chassis.min.js'))
+    s.setAttribute('src', require('path').join(__dirname, 'dist/' + component + '.min.js'))
     s.onload = typeof callback === 'function' ? function () { callback() } : function () {}
     document.head.appendChild(s)
   } else {
-    console.log('NGN Chassis failed to load: DOM does not exist or was not recognized.')
+    console.log('NGN Chassis failed to load ' + component + ': DOM does not exist or was not recognized.')
   }
 }
